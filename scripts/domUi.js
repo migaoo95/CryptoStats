d49faa1b4e8933407c0e2d58f56aa72829a60b1d
migaoo95 <45981784+migaoo95@users.noreply.github.com>
2021-12-16 19:33:55 +0000

    Price movement
    Percentage calculations added for the price movement

--- scripts/domUi.js
+++ scripts/domUi.js
@@ -2,7 +2,12 @@ class DomUI {
   constructor() {
     this.row = document.getElementById("row");
   }
+  static calcPrecentage(currency) {
+    const resultPrecentage =
+      Math.round(currency.price_change_percentage_24h * 100) / 100;
+  }
   createElemet(currency) {
+    console.log(Math.round(currency.price_change_percentage_24h * 100) / 100);
     this.row.innerHTML += `
     <div class="col-lg-3 col-md-6 mb-5">
     <div class="card">
@@ -13,11 +18,23 @@ class DomUI {
           class="img-fluid rounded-circle w-50 mb-3"
         />
         <h5>${currency.id}</h5>
-        <h5 class="text-muted">${currency.current_price} <span class="text-success">$</span></h5>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
-          commodi nostrum, ab libero voluptas officia.
-        </p>
+        <h5 class="">${
+          currency.current_price
+        } <span class="text-success">$</span></h5>
+        <h5><span>1h: </span> <span class="${
+          currency.price_change_percentage_24h >= 0
+            ? "text-success"
+            : "text-danger"
+        }">${
+      Math.round(currency.price_change_percentage_24h * 100) / 100
+    }%</span></h5>
+        <h5><span>24h: </span> <span class="${
+          currency.price_change_percentage_24h >= 0
+            ? "text-success"
+            : "text-danger"
+        }">${
+      Math.round(currency.price_change_percentage_24h * 100) / 100
+    }%</span></h5>
       
         </div>
       </div>
